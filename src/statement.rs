0132bd90356ca3c3c69baaa429ca41d527830a4f
Nik Deapen <nik@deapen.com>
2022-07-25 01:45:53 -0700

    Upgraded to version 0.1.0.

--- src/statement.rs
+++ src/statement.rs
@@ -1,6 +1,6 @@
 use crate::CodeBuffer;
 
-/// A code statement. (code that spans one more lines)
+/// A code statement. (code that spans one or more lines)
 pub trait Statement {
     /// Writes the statement to the code buffer at the indent level.
     fn write(&self, b: &mut CodeBuffer, level: usize);
