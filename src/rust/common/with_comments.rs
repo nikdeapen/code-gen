5e5107602104a86098cbbbc52daf38015093e631
Nik Deapen <nik@deapen.com>
2024-02-27 08:14:20 -0800

    [rust] Added function declarations.

---
+++ src/rust/common/with_comments.rs
@@ -0,0 +1,29 @@
+use crate::rust::CommentType;
+use crate::CodeBuffer;
+
+/// An element with comment lines.
+pub trait WithComments: Sized {
+    /// Gets the comments.
+    fn comments(&self) -> &[String];
+
+    /// Adds the comment.
+    fn with_comment<S>(mut self, comment: S) -> Self
+    where
+        S: Into<String>,
+    {
+        self.add_comment(comment);
+        self
+    }
+
+    /// Adds the comment.
+    fn add_comment<S>(&mut self, comment: S)
+    where
+        S: Into<String>;
+
+    /// Writes the comments.
+    fn write_comments(&self, comment_type: CommentType, b: &mut CodeBuffer, level: usize) {
+        for line in self.comments() {
+            comment_type.write_line(line.as_str(), b, level);
+        }
+    }
+}
---
+++ src/rust/function/function.rs
@@ -0,0 +1,121 @@
+use crate::rust::{Access, CommentType, Signature, WithAccess, WithComments, WithSignature};
+use crate::{CodeBuffer, Statement, WithStatements};
+
+/// A function declaration.
+pub struct Function {
+    comments: Vec<String>,
+    access: Access,
+    signature: Signature,
+    statements: Vec<Box<dyn Statement>>,
+}
+
+impl<S: Into<Signature>> From<S> for Function {
+    fn from(signature: S) -> Self {
+        Self {
+            comments: Vec::default(),
+            access: Access::default(),
+            signature: signature.into(),
+            statements: Vec::default(),
+        }
+    }
+}
+
+impl WithComments for Function {
+    fn comments(&self) -> &[String] {
+        self.comments.as_slice()
+    }
+
+    fn add_comment<S>(&mut self, comment: S)
+    where
+        S: Into<String>,
+    {
+        self.comments.push(comment.into());
+    }
+}
+
+impl WithAccess for Function {
+    fn access(&self) -> &Access {
+        &self.access
+    }
+
+    fn set_access<A>(&mut self, access: A)
+    where
+        A: Into<Access>,
+    {
+        self.access = access.into();
+    }
+}
+
+impl WithSignature for Function {
+    fn signature(&self) -> &Signature {
+        &self.signature
+    }
+}
+
+impl WithStatements for Function {
+    fn statements(&self) -> &[Box<dyn Statement>] {
+        self.statements.as_slice()
+    }
+
+    fn add_boxed_statement(&mut self, statement: Box<dyn Statement>) {
+        self.statements.push(statement);
+    }
+}
+
+impl Statement for Function {
+    fn write(&self, b: &mut CodeBuffer, level: usize) {
+        self.write_comments(CommentType::OuterLineDoc, b, level);
+        b.indent(level);
+        self.write_access(b);
+        b.write("fn ");
+        self.write_signature(b);
+        b.space();
+        self.write_curly_statement_block(b, level);
+        b.end_line();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::rust::Access::PublicInCrate;
+    use crate::rust::{Function, WithAccess, WithComments};
+    use crate::{CodeBuffer, Literal, WithStatements};
+
+    #[test]
+    fn write_empty() {
+        let function: Function = "myFn".into();
+        let result: String = CodeBuffer::display_statement(&function);
+        assert_eq!(result, "fn myFn() {}\n");
+    }
+
+    #[test]
+    fn write_comments() {
+        let function: Function = Function::from("myFn")
+            .with_comment("The first comment line.")
+            .with_comment("The second comment line.");
+        let result: String = CodeBuffer::display_statement(&function);
+        assert_eq!(
+            result,
+            "/// The first comment line.\n/// The second comment line.\nfn myFn() {}\n"
+        );
+    }
+
+    #[test]
+    fn write_access() {
+        let function: Function = Function::from("myFn").with_access(PublicInCrate);
+        let result: String = CodeBuffer::display_statement(&function);
+        assert_eq!(result, "pub(crate) fn myFn() {}\n");
+    }
+
+    #[test]
+    fn write_statements() {
+        let function: Function =
+            Function::from("myFn").with_expression_statement(Literal::from("one"));
+        let result: String = CodeBuffer::display_statement(&function);
+        assert_eq!(result, "fn myFn() {\n\tone\n}\n");
+
+        let function: Function = function.with_expression_statement(Literal::from("two"));
+        let result: String = CodeBuffer::display_statement(&function);
+        assert_eq!(result, "fn myFn() {\n\tone\n\ttwo\n}\n");
+    }
+}
