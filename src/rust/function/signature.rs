54ef3fc8578f22104770726a8957fc12d57afe80
Nik Deapen <nik@deapen.com>
2024-03-01 05:31:51 -0800

    [rust] Added support for generic functions. (#14)

--- src/rust/function/signature.rs
+++ src/rust/function/signature.rs
@@ -1,11 +1,13 @@
+use crate::rust::function::with_fn_generics::WithFnGenerics;
 use crate::rust::{Receiver, TypeTag, Var, WithReceiver, WithResult, WithVarParams};
 use crate::{CodeBuffer, Expression, ToStaticStr, WithName};
 
 /// A function signature.
 #[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
 pub struct Signature {
-    receiver: Option<Receiver>,
     name: String,
+    generics: Vec<Var>,
+    receiver: Option<Receiver>,
     params: Vec<Var>,
     result: Option<TypeTag>,
 }
@@ -13,11 +15,31 @@ pub struct Signature {
 impl<S: Into<String>> From<S> for Signature {
     fn from(name: S) -> Self {
         Self {
-            receiver: None,
-            result: None,
             name: name.into(),
+            generics: Vec::default(),
+            receiver: None,
             params: Vec::default(),
+            result: None,
         }
+    }
+}
+
+impl WithName for Signature {
+    fn name(&self) -> &str {
+        self.name.as_str()
+    }
+}
+
+impl WithFnGenerics for Signature {
+    fn generics(&self) -> &[Var] {
+        self.generics.as_slice()
+    }
+
+    fn add_generic<V>(&mut self, generic: V)
+    where
+        V: Into<Var>,
+    {
+        self.generics.push(generic.into());
     }
 }
 
@@ -28,12 +50,6 @@ impl WithReceiver for Signature {
 
     fn set_receiver(&mut self, receiver: Receiver) {
         self.receiver = Some(receiver)
-    }
-}
-
-impl WithName for Signature {
-    fn name(&self) -> &str {
-        self.name.as_str()
     }
 }
 
@@ -66,6 +82,15 @@ impl WithResult for Signature {
 impl Expression for Signature {
     fn write(&self, b: &mut CodeBuffer) {
         self.write_name(b);
+        if let Some((first, rest)) = self.generics().split_first() {
+            b.write("<");
+            first.write_name(b);
+            for generic in rest {
+                b.write(", ");
+                generic.write_name(b);
+            }
+            b.write(">")
+        }
         b.write("(");
         if let Some(receiver) = self.receiver {
             b.write(receiver.to_static_str());
@@ -76,11 +101,20 @@ impl Expression for Signature {
         self.write_params(b);
         b.write(")");
         self.write_result(b);
+        if let Some((first, rest)) = self.generics().split_first() {
+            b.write(" where ");
+            first.write(b);
+            for generic in rest {
+                b.write(", ");
+                generic.write(b);
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::rust::function::with_fn_generics::WithFnGenerics;
     use crate::rust::function::with_result::WithResult;
     use crate::rust::PrimitiveType::{UnsignedInt16, UnsignedInt32, UnsignedInt8};
     use crate::rust::{Receiver, Signature, WithReceiver, WithVarParams};
@@ -128,5 +162,16 @@ mod tests {
         let signature: Signature = signature.with_param(("two", UnsignedInt16));
         let result: String = CodeBuffer::display_expression(&signature);
         assert_eq!(result, "myFn(&mut self, one: u8, two: u16)");
+    }
+
+    #[test]
+    fn write_generics() {
+        let signature: Signature = Signature::from("myFn").with_generic(("T", "MyType"));
+        let result: String = CodeBuffer::display_expression(&signature);
+        assert_eq!(result, "myFn<T>() where T: MyType");
+
+        let signature: Signature = signature.with_generic(("B", "BeeType"));
+        let result: String = CodeBuffer::display_expression(&signature);
+        assert_eq!(result, "myFn<T, B>() where T: MyType, B: BeeType");
     }
 }
