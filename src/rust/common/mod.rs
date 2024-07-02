4f4ea1c6ef8e29217954742914c374356cbddec9
Nik Deapen <nik@deapen.com>
2024-07-02 09:03:58 -0700

    [rust] Added support for access levels.

--- src/rust/common/mod.rs
+++ src/rust/common/mod.rs
@@ -1,7 +1,11 @@
 pub use comment_type::*;
 pub use with_comments::*;
 pub use with_unsafe_flag::*;
+pub use access::*;
+pub use with_access::*;
 
+mod access;
 mod comment_type;
 mod with_comments;
 mod with_unsafe_flag;
+mod with_access;
