8f41c1a321112ef87e91aad2aceedd8c41b272f9
Nik Deapen <nik@deapen.com>
2024-07-02 09:14:09 -0700

    [rust] Added support for setter functions.

--- src/rust/function/util/mod.rs
+++ src/rust/function/util/mod.rs
@@ -1,5 +1,9 @@
 pub use gen_getter_copy::*;
 pub use gen_getter_field_exp::*;
+pub use gen_setter_copy::*;
+pub use gen_setter_mem_replace::*;
 
 mod gen_getter_copy;
 mod gen_getter_field_exp;
+mod gen_setter_copy;
+mod gen_setter_mem_replace;
