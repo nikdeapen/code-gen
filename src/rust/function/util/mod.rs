eb27c1d618be94692e926d7f36c06086f792c113
Nik Deapen <nik@deapen.com>
2024-07-02 09:14:36 -0700

    [rust] Added support for builder functions.

--- src/rust/function/util/mod.rs
+++ src/rust/function/util/mod.rs
@@ -1,8 +1,10 @@
+pub use gen_builder::*;
 pub use gen_getter_copy::*;
 pub use gen_getter_field_exp::*;
 pub use gen_setter_copy::*;
 pub use gen_setter_mem_replace::*;
 
+mod gen_builder;
 mod gen_getter_copy;
 mod gen_getter_field_exp;
 mod gen_setter_copy;
