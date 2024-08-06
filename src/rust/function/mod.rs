8750d96aaf633e8f1ec2c432ea9ae6f20f1be07f
Nik Deapen <nik@deapen.com>
2024-08-06 16:18:08 -0700

    [rust] Added function declarations.

--- src/rust/function/mod.rs
+++ src/rust/function/mod.rs
@@ -1,11 +1,15 @@
+pub use function::*;
 pub use receiver::*;
 pub use signature::*;
 pub use with_fn_generics::*;
 pub use with_receiver::*;
 pub use with_result::*;
+pub use with_signature::*;
 
+mod function;
 mod receiver;
 mod signature;
 mod with_fn_generics;
 mod with_receiver;
 mod with_result;
+mod with_signature;
