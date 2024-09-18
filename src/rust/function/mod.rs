ed9a650d26ed2ca94bbca954eae8702bbe41de04
Nik Deapen <nik@deapen.com>
2024-09-18 09:51:48 -0700

    [rust] Added support for function signatures.

--- src/rust/function/mod.rs
+++ src/rust/function/mod.rs
@@ -1,7 +1,11 @@
 pub use receiver::*;
+pub use signature::*;
 pub use with_function_generics::*;
 pub use with_receiver::*;
+pub use with_result::*;
 
 mod receiver;
+mod signature;
 mod with_function_generics;
 mod with_receiver;
+mod with_result;
