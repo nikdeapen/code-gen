b9d91ee2d6582993697816c5b3b46c84b89260df
Nik Deapen <nik@deapen.com>
2024-09-18 10:13:07 -0700

    [rust] Added support for if statements.

--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -1,10 +1,12 @@
 pub use common::*;
+pub use control::*;
 pub use exp::*;
 pub use function::*;
 pub use structure::*;
 pub use var::*;
 
 mod common;
+mod control;
 mod exp;
 mod function;
 mod structure;
