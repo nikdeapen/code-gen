6b5c04d89bebc9b6615b33ade407deab774e07cf
Nik Deapen <nik@deapen.com>
2024-09-18 10:00:34 -0700

    [rust] Added support for struct declarations.

--- src/rust/var/mod.rs
+++ src/rust/var/mod.rs
@@ -3,6 +3,7 @@ pub use reference::*;
 pub use type_tag::*;
 pub use var::*;
 pub use with_type_tag::*;
+pub use with_var::*;
 pub use with_var_params::*;
 
 mod primitive_type;
@@ -10,4 +11,5 @@ mod reference;
 mod type_tag;
 mod var;
 mod with_type_tag;
+mod with_var;
 mod with_var_params;
