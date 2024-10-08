1d8218531259b50f1042008c9612f33b51edd47b
Nik Deapen <nik@deapen.com>
2024-10-08 12:53:06 -0700

    [rust] Added support for variables.

--- src/rust/var/mod.rs
+++ src/rust/var/mod.rs
@@ -1,7 +1,13 @@
 pub use primitive_type::*;
 pub use reference::*;
 pub use type_tag::*;
+pub use var::*;
+pub use with_type_tag::*;
+pub use with_var::*;
 
 mod primitive_type;
 mod reference;
 mod type_tag;
+mod var;
+mod with_type_tag;
+mod with_var;
