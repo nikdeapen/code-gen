ee02e9a39ab12b904ac5756a49e6ec5860987be4
Nik Deapen <nik@deapen.com>
2024-10-09 10:15:34 -0700

    [rust] Added support for enum fields.

--- src/rust/enums/enum_case.rs
+++ src/rust/enums/enum_case.rs
@@ -1,10 +1,11 @@
-use crate::rust::{CommentType, WithComments};
-use crate::{CodeBuffer, Statement, WithName};
+use crate::rust::{CommentType, EnumFields, WithComments};
+use crate::{CodeBuffer, Expression, Statement, WithName};
 
 /// An enum case.
 pub struct EnumCase {
     comments: Vec<String>,
     name: String,
+    fields: EnumFields,
 }
 
 impl<S: Into<String>> From<S> for EnumCase {
@@ -12,6 +13,7 @@ impl<S: Into<String>> From<S> for EnumCase {
         Self {
             comments: Vec::default(),
             name: name.into(),
+            fields: EnumFields::default(),
         }
     }
 }
@@ -35,11 +37,38 @@ impl WithName for EnumCase {
     }
 }
 
+impl EnumCase {
+    //! Fields
+
+    /// Gets the fields.
+    pub fn fields(&self) -> &EnumFields {
+        &self.fields
+    }
+
+    /// Sets the fields.
+    pub fn set_fields<F>(&mut self, fields: F)
+    where
+        F: Into<EnumFields>,
+    {
+        self.fields = fields.into();
+    }
+
+    /// Sets the fields.
+    pub fn with_fields<F>(mut self, fields: F) -> Self
+    where
+        F: Into<EnumFields>,
+    {
+        self.set_fields(fields);
+        self
+    }
+}
+
 impl Statement for EnumCase {
     fn write(&self, b: &mut CodeBuffer, level: usize) {
         self.write_comments(CommentType::OuterLineDoc, b, level);
         b.indent(level);
         self.write_name(b);
+        self.fields.write(b);
         b.write(",");
         b.end_line();
     }
