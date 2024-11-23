571e1dfc7e4488be6c5af1a14a7f4feca867c28f
Nik Deapen <nik@deapen.com>
2024-11-23 05:57:39 -0800

    [rust] Added support for loops.

---
+++ src/rust/control/loops/mod.rs
@@ -0,0 +1,5 @@
+pub use forever_loop::*;
+pub use while_loop::*;
+
+mod forever_loop;
+mod while_loop;
--- src/rust/control/mod.rs
+++ src/rust/control/mod.rs
@@ -1,9 +1,11 @@
 pub use if_statement::*;
+pub use loops::*;
 pub use match_case::*;
 pub use match_statement::*;
 pub use unsafe_block::*;
 
 mod if_statement;
+mod loops;
 mod match_case;
 mod match_statement;
 mod unsafe_block;
