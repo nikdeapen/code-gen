f1f6b98d86b435122bfb59e00633e8f4ce58bd82
Nik Deapen <nik@deapen.com>
2024-11-23 06:00:09 -0800

    [rust] Added support for field expressions.

--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -1,6 +1,7 @@
 pub use common::*;
 pub use control::*;
 pub use enums::*;
+pub use exp::*;
 pub use function::*;
 pub use structs::*;
 pub use var::*;
@@ -8,6 +9,7 @@ pub use var::*;
 mod common;
 mod control;
 mod enums;
+mod exp;
 mod function;
 mod structs;
 mod var;
