5534b66a07027660bf8d80284915adc7d549454f
Nik Deapen <nik@deapen.com>
2024-11-23 05:47:29 -0800

    [rust] Added support for structs.

--- src/rust/common/mod.rs
+++ src/rust/common/mod.rs
@@ -2,10 +2,12 @@ pub use access::*;
 pub use comment_type::*;
 pub use with_access::*;
 pub use with_comments::*;
+pub use with_derives::*;
 pub use with_unsafe_flag::*;
 
 mod access;
 mod comment_type;
 mod with_access;
 mod with_comments;
+mod with_derives;
 mod with_unsafe_flag;
--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -1,7 +1,9 @@
 pub use common::*;
 pub use function::*;
+pub use structs::*;
 pub use var::*;
 
 mod common;
 mod function;
+mod structs;
 mod var;
