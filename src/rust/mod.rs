413e09a96b1c7f0c50d9cb1eb86e5a59e4126ce0
Nik Deapen <nik@deapen.com>
2025-02-12 11:23:09 -0800

    [rust] Added support for impl blocks.

--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -1,7 +1,9 @@
 pub use common::*;
 pub use function::*;
+pub use types::*;
 pub use var::*;
 
 mod common;
 mod function;
+mod types;
 mod var;
---
+++ src/rust/types/mod.rs
@@ -0,0 +1,7 @@
+pub use impl_block::*;
+pub use type_dec::*;
+pub use with_type_decs::*;
+
+mod impl_block;
+mod type_dec;
+mod with_type_decs;
