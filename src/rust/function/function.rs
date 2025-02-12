a9811cbb89b10b88691d23401bc0794ab989508f
Nik Deapen <nik@deapen.com>
2025-02-12 11:17:19 -0800

    [rust] Added support for functions & calls.

---
+++ src/rust/function/function.rs
@@ -0,0 +1,77 @@
+use crate::rust::CommentType::OuterLineDoc;
+use crate::rust::{Access, Signature, WithAccess, WithComments, WithSignature};
+use crate::{CodeBuffer, Statement, WithStatements};
+
+/// A function declaration.
+pub struct Function {
+    comments: Vec<String>,
+    access: Access,
+    signature: Signature,
+    statements: Vec<Box<dyn Statement>>,
+}
+
+impl<S: Into<Signature>> From<S> for Function {
+    fn from(signature: S) -> Self {
+        Self {
+            comments: Vec::default(),
+            access: Access::default(),
+            signature: signature.into(),
+            statements: Vec::default(),
+        }
+    }
+}
+
+impl WithComments for Function {
+    fn comments(&self) -> &[String] {
+        self.comments.as_slice()
+    }
+
+    fn add_comment<S>(&mut self, comment: S)
+    where
+        S: Into<String>,
+    {
+        self.comments.push(comment.into());
+    }
+}
+
+impl WithAccess for Function {
+    fn access(&self) -> &Access {
+        &self.access
+    }
+
+    fn set_access<A>(&mut self, access: A)
+    where
+        A: Into<Access>,
+    {
+        self.access = access.into();
+    }
+}
+
+impl WithSignature for Function {
+    fn signature(&self) -> &Signature {
+        &self.signature
+    }
+}
+
+impl WithStatements for Function {
+    fn statements(&self) -> &[Box<dyn Statement>] {
+        self.statements.as_slice()
+    }
+
+    fn add_boxed_statement(&mut self, statement: Box<dyn Statement>) {
+        self.statements.push(statement);
+    }
+}
+
+impl Statement for Function {
+    fn write(&self, b: &mut CodeBuffer, level: usize) {
+        self.write_comments(OuterLineDoc, b, level);
+        b.indent(level);
+        self.write_access(b);
+        b.write("fn ");
+        self.write_signature(b);
+        b.space();
+        self.write_curly_statement_block(b, level);
+        b.end_line();
+    }
+}
