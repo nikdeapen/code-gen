23e9fd005efac884b51658a601ee392cd1412631
Nik Deapen <nik@deapen.com>
2025-02-12 11:26:09 -0800

    [rust] Added support for enum declarations.

---
+++ src/rust/types/enums/mod.rs
@@ -0,0 +1,7 @@
+pub use enum_case::*;
+pub use enum_fields::*;
+pub use enumeration::*;
+
+mod enum_case;
+mod enum_fields;
+mod enumeration;
--- src/rust/types/mod.rs
+++ src/rust/types/mod.rs
@@ -1,9 +1,11 @@
+pub use enums::*;
 pub use impl_block::*;
 pub use structs::*;
 pub use type_dec::*;
 pub use with_derives::*;
 pub use with_type_decs::*;
 
+mod enums;
 mod impl_block;
 mod structs;
 mod type_dec;
