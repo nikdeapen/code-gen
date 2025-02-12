dbea39fbdb5e1750b3be304ef71269ca86f2b5e8
Nik Deapen <nik@deapen.com>
2025-02-12 11:29:44 -0800

    [rust] Added support for field expressions.

---
+++ src/rust/exp/mod.rs
@@ -0,0 +1,5 @@
+pub use field_exp::*;
+pub use set_field_exp::*;
+
+mod field_exp;
+mod set_field_exp;
--- src/rust/mod.rs
+++ src/rust/mod.rs
@@ -1,11 +1,13 @@
 pub use common::*;
 pub use control::*;
+pub use exp::*;
 pub use function::*;
 pub use types::*;
 pub use var::*;
 
 mod common;
 mod control;
+mod exp;
 mod function;
 mod types;
 mod var;
