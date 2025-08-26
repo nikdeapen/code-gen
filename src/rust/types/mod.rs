e56dbbe336a760c070da0adf1423562464652a9a
Nik Deapen <nik@deapen.com>
2025-08-26 10:16:26 -0700

    [rust] Added support for trait declarations.

--- src/rust/types/mod.rs
+++ src/rust/types/mod.rs
@@ -1,6 +1,7 @@
 pub use impl_block::*;
 pub use r#enum::*;
 pub use r#struct::*;
+pub use r#trait::*;
 pub use type_dec::*;
 pub use with_derives::*;
 pub use with_type_decs::*;
@@ -8,6 +9,7 @@ pub use with_type_decs::*;
 mod r#enum;
 mod impl_block;
 mod r#struct;
+mod r#trait;
 mod type_dec;
 mod with_derives;
 mod with_type_decs;
---
+++ src/rust/types/trait/mod.rs
@@ -0,0 +1,5 @@
+pub use r#trait::*;
+pub use with_trait_functions::*;
+
+mod r#trait;
+mod with_trait_functions;
