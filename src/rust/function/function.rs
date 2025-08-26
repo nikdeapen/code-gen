24311fa2a0dd719ba47a5fe2bff82e5dd629e976
Nik Deapen <nik@deapen.com>
2025-08-26 10:07:20 -0700

    [rust] Added support for function declarations.

---
+++ src/rust/function/function.rs
@@ -0,0 +1,116 @@
+use crate::rust::CommentType::OuterLineDoc;
+use crate::rust::{
+    Access, Signature, WithAccess, WithAttributes, WithComments, WithSignature, WithUnsafeFlag,
+};
+use crate::{CodeBuffer, Statement, WithStatements};
+
+/// A function declaration.
+pub struct Function {
+    comments: Vec<String>,
+    attributes: Vec<String>,
+    is_async: bool,
+    access: Access,
+    signature: Signature,
+    statements: Vec<Box<dyn Statement>>,
+}
+
+impl<S: Into<Signature>> From<S> for Function {
+    fn from(signature: S) -> Self {
+        Self {
+            comments: Vec::default(),
+            attributes: Vec::default(),
+            is_async: false,
+            access: Access::default(),
+            signature: signature.into(),
+            statements: Vec::default(),
+        }
+    }
+}
+
+impl WithComments for Function {
+    fn comments(&self) -> &[String] {
+        self.comments.as_slice()
+    }
+
+    fn add_comment<S>(&mut self, comment: S)
+    where
+        S: Into<String>,
+    {
+        self.comments.push(comment.into());
+    }
+}
+
+impl WithAttributes for Function {
+    fn attributes(&self) -> &[String] {
+        self.attributes.as_slice()
+    }
+
+    fn add_attribute<S>(&mut self, attribute: S)
+    where
+        S: Into<String>,
+    {
+        self.attributes.push(attribute.into());
+    }
+}
+
+impl WithAccess for Function {
+    fn access(&self) -> &Access {
+        &self.access
+    }
+
+    fn set_access<A>(&mut self, access: A)
+    where
+        A: Into<Access>,
+    {
+        self.access = access.into();
+    }
+}
+
+impl Function {
+    //! Async
+
+    /// Sets the `is_async` flag.
+    pub fn set_async(&mut self, is_async: bool) {
+        self.is_async = is_async;
+    }
+
+    /// Sets the `is_async` flag.
+    pub fn with_async(mut self, is_async: bool) -> Self {
+        self.set_async(is_async);
+        self
+    }
+}
+
+impl WithSignature for Function {
+    fn signature(&self) -> &Signature {
+        &self.signature
+    }
+}
+
+impl WithStatements for Function {
+    fn statements(&self) -> &[Box<dyn Statement>] {
+        self.statements.as_slice()
+    }
+
+    fn add_boxed_statement(&mut self, statement: Box<dyn Statement>) {
+        self.statements.push(statement);
+    }
+}
+
+impl Statement for Function {
+    fn write(&self, b: &mut CodeBuffer, level: usize) {
+        self.write_comments(OuterLineDoc, b, level);
+        self.write_attributes(b, level);
+        b.indent(level);
+        self.write_access(b);
+        if self.is_async {
+            b.write(" async");
+        }
+        self.signature.write_unsafe(b);
+        b.write("fn ");
+        self.write_signature(b);
+        b.space();
+        self.write_curly_statement_block(b, level);
+        b.end_line();
+    }
+}
