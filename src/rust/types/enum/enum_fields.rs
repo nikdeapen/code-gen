a994629bcb70cbd9c253a87926d2119934fcb221
Nik Deapen <nik@deapen.com>
2025-08-26 10:15:11 -0700

    [rust] Added support for enum declarations.

---
+++ src/rust/types/enum/enum_fields.rs
@@ -0,0 +1,45 @@
+use crate::rust::{RustType, Var};
+use crate::{CodeBuffer, Expression};
+
+/// Enum fields.
+pub enum EnumFields {
+    Empty,
+    Named(Vec<Var>),
+    Unnamed(Vec<RustType>),
+}
+
+impl Default for EnumFields {
+    fn default() -> Self {
+        Self::Empty
+    }
+}
+
+impl Expression for EnumFields {
+    fn write(&self, b: &mut CodeBuffer) {
+        match self {
+            Self::Empty => {}
+            Self::Named(vars) => {
+                if let Some((first, rest)) = vars.split_first() {
+                    b.write("{ ");
+                    first.write(b);
+                    for var in rest {
+                        b.write(", ");
+                        var.write(b);
+                    }
+                    b.write(" }");
+                }
+            }
+            Self::Unnamed(tags) => {
+                if let Some((first, rest)) = tags.split_first() {
+                    b.write("(");
+                    first.write(b);
+                    for var in rest {
+                        b.write(", ");
+                        var.write(b);
+                    }
+                    b.write(")");
+                }
+            }
+        }
+    }
+}
---
+++ src/rust/types/enum/mod.rs
@@ -0,0 +1,7 @@
+pub use enum_case::*;
+pub use enum_fields::*;
+pub use r#enum::*;
+
+mod r#enum;
+mod enum_case;
+mod enum_fields;
