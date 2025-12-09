eae3da1b6d078fae14cc44513f73c63cb797d014
Nik Deapen <nik@deapen.com>
2025-12-09 13:19:26 -0800

    [rust] Added support for variables.

--- src/rust/var/mod.rs
+++ src/rust/var/mod.rs
@@ -1,7 +1,17 @@
+pub use const_init::*;
 pub use reference::*;
 pub use rust_primitive::*;
 pub use rust_type::*;
+pub use var::*;
+pub use var_init::*;
+pub use with_rust_type::*;
+pub use with_var::*;
 
+mod const_init;
 mod reference;
 mod rust_primitive;
 mod rust_type;
+mod var;
+mod var_init;
+mod with_rust_type;
+mod with_var;
