33cf9e8182920ebc7af54b4a15d229bdb98a4d15
Nik Deapen <nik@deapen.com>
2025-12-09 13:31:18 -0800

    [rust] Added support for struct declarations.

--- src/rust/types/mod.rs
+++ src/rust/types/mod.rs
@@ -1,7 +1,11 @@
 pub use impl_block::*;
+pub use r#struct::*;
 pub use type_dec::*;
+pub use with_derives::*;
 pub use with_type_decs::*;
 
 mod impl_block;
+mod r#struct;
 mod type_dec;
+mod with_derives;
 mod with_type_decs;
